a804280e2c7a0d93448543b61212eb14178c6238
brunatguerra <brunatguerra@gmail.com>
2024-10-08 18:21:09 -0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        // TODO: Realizar os testes com as classes Nokia e Iphone
+        {
+            Smartphone nokia = new Nokia("123456", "4242", "1234", 20);
+            nokia.InstalarAplicativo("WhatsApp");
+
+            // Teste com a classe Iphone
+            Smartphone iphone = new Iphone("654321", "4343", "4321", 25);
+            iphone.InstalarAplicativo("Instagram");
+
+        }
+    }
+}
